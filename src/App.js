ce91b4cbddc6af67d48be5251251b1f6d44cae11
Pouriya Jp <100994285+PouriyaJp@users.noreply.github.com>
2023-07-22 16:22:40 +0330

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {ThemeProvider, createTheme} from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import {CacheProvider} from "@emotion/react";
+import {HelmetProvider, Helmet} from "react-helmet-async";
+import createCache from "@emotion/cache";
+import {prefixer} from "stylis";
+import {} from "@mui/material";
+import "./App.css";
+
+// Create Theme
+const theme = createTheme({
+    direction: "rtl",
+    typography: {
+      fontFamily: "vazir, roboto"
+    }
+});
+// Create Rtl Cache
+const cacheRTL = createCache({
+    key: "muirtl",
+    stylisPlugins: [prefixer, rtlPlugin]
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <CacheProvider value={cacheRTL}>
+            <ThemeProvider theme={theme}>
+                <HelmetProvider>
+                    <Helmet>
+                        <title>وب سایت شخصی پوریا جمشیدپور</title>
+                    </Helmet>
+                    <div className="App">
+
+                    </div>
+                </HelmetProvider>
+            </ThemeProvider>
+        </CacheProvider>
+    );
 }
 
 export default App;
